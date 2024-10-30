19ef56fca0bf2464a9099aa4215f875e1a7b1033
Shaposhnikov-Nick <shaposhnikov.nick@gmail.com>
2024-10-30 15:01:29 +0300

    fetch data

--- next.js course/next-js/src/pages/contacts/index.js
+++ next.js course/next-js/src/pages/contacts/index.js
@@ -1,9 +1,49 @@
+import { useState, useEffect } from "react";
 import Heading from "@/components/Heading";
+import { notFound } from "next/navigation";
 
-const Contacts = () => {
+// Так получают данные в Next.js приложении
+// и передают их как props для компонента
+export const getStaticProps = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+
+  // Специальная проверка если запрос неуспешный
+  if (!data) {
+    return {
+      notFound: true,
+    };
+  }
+  return {
+    props: { contacts: data },
+  };
+};
+
+const Contacts = ({ contacts }) => {
+  // Так получают данные в обычном React приложении:
+  /*
+  const [contacts, setContacts] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      const data = await response.json();
+      setContacts(data);
+    };
+    fetchData();
+  }, []);
+  */
+
   return (
     <>
       <Heading text='Contacts list:' />
+      <ul>
+        {contacts &&
+          contacts.map(({ id, name, email }) => (
+            <li key={id}>{` name: ${name}, email: ${email}`}</li>
+          ))}
+      </ul>
     </>
   );
 };
