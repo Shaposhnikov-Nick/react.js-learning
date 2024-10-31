162af6b6b09d03c1963180366dec70b96d09ffb6
Shaposhnikov-Nick <shaposhnikov.nick@gmail.com>
2024-10-31 12:34:37 +0300

    API creating

--- next.js course/next-js/src/pages/index.js
+++ next.js course/next-js/src/pages/index.js
@@ -1,10 +1,30 @@
 import Heading from "@/components/Heading";
+import Socials from "@/components/Socials";
 import styles from "../styles/Home.module.scss";
 
-const Home = () => (
-  <div className={styles.wrapper}>
-    <Heading text='Hello world' />
-  </div>
-);
+export const getStaticProps = async () => {
+    const response = await fetch(`${process.env.API_HOST}/socials`);
+    const data = await response.json();
+
+    if (!data) {
+        return {
+            notFound: true,
+        };
+    }
+    return {
+        props: {socials: data},
+    };
+};
+
+const Home = ({socials}) => {
+    console.log(socials)
+    return (
+        <div className={styles.wrapper}>
+            <Heading text='Home'/>
+            <Socials socials={socials.socials}/>
+        </div>
+    )
+
+};
 
 export default Home;
