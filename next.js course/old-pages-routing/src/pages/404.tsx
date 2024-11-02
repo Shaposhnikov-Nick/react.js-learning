2523df84308ca0c9c3891dc866453ad7d7e9231a
Shaposhnikov-Nick <shaposhnikov.nick@gmail.com>
2024-11-02 16:17:02 +0300

    add 404

---
+++ next.js course/old-pages-routing/src/pages/404.tsx
@@ -0,0 +1,12 @@
+import {FC} from 'react'
+import Layout from "@/components/layout/Layout";
+
+const NotFound: FC = () => {
+    return (
+        <Layout>
+            Page not found
+        </Layout>
+    )
+}
+
+export default NotFound
