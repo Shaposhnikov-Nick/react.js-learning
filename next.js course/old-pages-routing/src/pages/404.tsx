3be21558f83204fb49ba307232f62fe94a56d049
Shaposhnikov-Nick <shaposhnikov.nick@gmail.com>
2024-11-06 15:04:15 +0300

    update

--- next.js course/old-pages-routing/src/pages/404.tsx
+++ next.js course/old-pages-routing/src/pages/404.tsx
@@ -1,10 +1,16 @@
 import {FC} from 'react'
 import Layout from "@/components/layout/Layout";
+import {Head} from "next/document";
 
 const NotFound: FC = () => {
     return (
         <Layout>
-            Page not found
+            {/*<Head>*/}
+            {/*    <title>Not Found</title>*/}
+            {/*</Head>*/}
+            <div style={{textAlign: 'center'}}>
+                Page not found
+            </div>
         </Layout>
     )
 }
