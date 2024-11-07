7bcdc8608faf1e638be159a0f742a85a1ef4aec1
Shaposhnikov-Nick <shaposhnikov.nick@gmail.com>
2024-11-07 12:36:53 +0300

    fetch (SSR, SSG, ISG)

--- next.js course/next-js-13/src/app/(lk)/dashboard/profile/page.tsx
+++ next.js course/next-js-13/src/app/(lk)/dashboard/profile/page.tsx
@@ -1,8 +1,18 @@
-import {FC} from "react";
 
-const ProfilePage: FC = () => {
+const fetchData = () => {
+    return fetch('https://jsonplaceholder.typicode.com/todos/1', {
+        // cache: "no-store"     // SSR
+        // cache: "force-cache"  // SSG
+        next: {revalidate: 10}   // ISG ревалидация каждые 10 с
+    }).then(res => res.json())
+}
+
+const ProfilePage = async () => {
+    const data = await fetchData()
+    console.log(data)
+
     return (
-        <div>ProfilePage</div>
+        <div>{data.id}</div>
     )
 }
 
